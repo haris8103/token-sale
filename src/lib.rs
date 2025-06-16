31b810232cfb65aeb66e580bf22c7642848d73da
haris8103 <91959015+haris8103@users.noreply.github.com>
2025-06-16 13:00:22 +0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,23 @@
-use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
-};
+use anchor_lang::prelude::*;
 
-pub mod instruction;
 pub mod processor;
-pub mod state;
+pub use processor::*;
 
-entrypoint!(process_instruction);
+declare_id!("kJzY1czYx9MZZ1Djjr3Npk34ahmm6BBCNJqZy4gfn7r");
 
-fn process_instruction(
-    program_id: &Pubkey,
-    accounts: &[AccountInfo],
-    instruction_data: &[u8],
-) -> ProgramResult {
-    processor::process_instruction(program_id, accounts, instruction_data)
+#[program]
+pub mod token_sale_program {
+    use super::*;
+
+    pub fn create(ctx: Context<CreateToken>, args: CreateTokenArgs) -> Result<()> {
+        processor::create(ctx, args)
+    }
+
+    pub fn first_buy(ctx: Context<FirstBuy>, args: FirstBuyArgs) -> Result<()> {
+        processor::first_buy(ctx, args)
+    }
+
+    pub fn buy(ctx: Context<Buy>, args: BuyArgs) -> Result<()> {
+        processor::buy(ctx, args)
+    }
 }
